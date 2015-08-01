fc2a4d3de82fe4e752a589ea666eaab1bc379c10
Cheng,Zhang <459309661@163.com>
2015-08-01 11:02:38 +0800

    Revert "Add [Python] Summary Range"
    This reverts commit dde63d74e5d024fdc8034b7d13564e0b75489b48.

--- LeetCode/python/SummaryRange.py
+++ LeetCode/python/SummaryRange.py
@@ -6,14 +6,13 @@ class Solution:
         rangeStr = str(start)
         if(end != start): #范围的起始和结尾整数不同
             rangeStr += "->"+str(end)
-            
-        self.resultRange.append(rangeStr)
+            self.resultRange.append(rangeStr)
                     
     # @param {integer[]} nums
     # @return {string[]}
     def summaryRanges(self, nums):
         if(len(nums) == 0):
-            return []
+            return [""]
 
         self.resultRange = [] #返回的范围列表
         startInt = nums[0] #范围的起始整数
@@ -21,17 +20,9 @@ class Solution:
             
         for i in range(1,len(nums)):
             if(endInt+1 < nums[i]): #范围的结束整数已找到
-                self.addRange(startInt,endInt)
+
                 startInt = nums[i]
             
             endInt = nums[i]    
-        
-        self.addRange(startInt,endInt)
-        
-        return self.resultRange
- 
-#main 函数
-s = Solution()
-nums = [-2,-1,0,1,2,3,4,5,6,8,10]
-print(s.summaryRanges(nums))
- 
+                
+        return resultRange    
