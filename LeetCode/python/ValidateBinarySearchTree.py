ea4a9f13e84846b917ea08c8a8a39021e9a62541
Cheng,Zhang <459309661@163.com>
2016-03-30 13:33:44 +0800

    Update Validate Binary Search Tree
    Update Validate Binary Search Tree python version

--- LeetCode/python/ValidateBinarySearchTree.py
+++ LeetCode/python/ValidateBinarySearchTree.py
@@ -1,12 +1,11 @@
 # Definition for a binary tree node.
-import sys
-
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
+#限定范围来比较节点的值是否在其中
 class Solution(object):
     def validBSTHelper(self,root,min,max):
         if(root == None):
@@ -26,6 +25,45 @@ class Solution(object):
             return True
             
         return self.validBSTHelper(root.left,-10000000000,root.val) and self.validBSTHelper(root.right,root.val,10000000000)
+
+#利用中序遍历后的链表来比较是否递增
+class Solution(object):
+    def inorderTraverse(self,root,lst):
+        if(root == None):
+            return lst
+        
+        lst = self.inorderTraverse(root.left,lst)
+        lst.append(root.val)
+        lst = self.inorderTraverse(root.right,lst)
+        
+        return lst
+    
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if(root == None):
+            return True
+        
+        treelst = self.inorderTraverse(root,[])
+        
+        print(treelst)
+        
+        lstSize = len(treelst)
+        item = treelst[0]
+        
+        result = True
+        
+        for i in range(1,lstSize):
+            treeItem = treelst[i]
+            if(item>=treeItem):
+                result = False
+                break
+            else:
+                item = treeItem
+                
+        return result
         
 #测试程序
 s = Solution()
