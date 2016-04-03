7f824d420ed6bacb298838e1df53dd648ae8d312
Cheng,Zhang <459309661@163.com>
2016-04-03 17:07:02 +0800

    Add Sum Root To Lead Numbers
    Add Sum Root To Lead Numbers C++ version

---
+++ LeetCode/c++/SumRootToLeafNumbers.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}	
+};
+
+int getSum(vector<int> v)
+{
+	int sum = 0;
+	int vSize = v.size()-1;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		int j = i;
+		int tmp = v[i];
+		while (j < vSize)
+		{
+			tmp *= 10;
+			j++;
+		}
+
+		sum += tmp;
+	}
+
+	return sum;
+}
+
+int sumNumbers(TreeNode* root) {
+	int sum = 0;
+	if (root != NULL)
+	{
+		stack<TreeNode*> s;
+		s.push(root);
+
+		vector<int> rootv;
+		rootv.push_back(root->val);
+
+		stack<vector<int>> sumS;
+		sumS.push(rootv);
+
+		while (!s.empty())
+		{
+			TreeNode *node = s.top();
+			s.pop();
+
+			vector<int> v = sumS.top();
+			sumS.pop();
+			
+			if (node->right != NULL)
+			{
+				s.push(node->right);
+
+				vector<int> leftV = v;
+				leftV.push_back(node->right->val);
+				sumS.push(leftV);
+			}
+			
+			if (node->left != NULL)
+			{
+				s.push(node->left);
+				vector<int> rightV = v;
+				rightV.push_back(node->left->val);
+				sumS.push(rightV);
+			}
+
+			if (node->right == NULL && node->left == NULL)//树的叶子节点
+			{
+				int leafSum = getSum(v);
+				sum += leafSum;
+			}
+		}
+	}
+
+	return sum;
+}
+
+int main()
+{
+	TreeNode *root = new TreeNode(1);
+	TreeNode *node1 = new TreeNode(2);
+	TreeNode *node2 = new TreeNode(3);
+
+	TreeNode *node3 = new TreeNode(4);
+	TreeNode *node4 = new TreeNode(5);
+	TreeNode *node6 = new TreeNode(6);
+
+	root->left = node1;
+	root->right = node2;
+
+	node1->left = node3;
+	node1->right = node4;
+
+	node2->right = node6;
+
+
+	cout << sumNumbers(root);
+
+	return 0;
+}
