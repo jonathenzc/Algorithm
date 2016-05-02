8033cf1601d239a2b6306da7cd14c7d55da62ed5
Cheng,Zhang <459309661@163.com>
2016-05-02 11:31:16 +0800

    Add Reverse String
    Add Reverse String C++ version

---
+++ LeetCode/c++/ReverseString.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string reverseString(string s) {
+	if (s.size() == 0 || s.size() == 1)
+		return s;
+
+	int end = s.size() - 1;
+
+	for (int i = 0;i<s.size()/2;i++)
+	{
+		char tmp = s[i];
+		s[i] = s[end];
+		s[end] = tmp;
+
+		end--;
+	}
+
+	return s;
+}
+
+int main()
+{
+	cout << reverseString("") << endl
+		<< reverseString("a") << endl
+		<< reverseString("abc") << endl
+		<< reverseString("abcd") << endl;
+
+	return 0;
+}
