8c8e58e9587d1e372aeec0e763dab41791afdf2b
Cheng,Zhang <459309661@163.com>
2016-06-16 16:18:58 +0800

    update SimplifyPath C++ version

--- LeetCode/c++/SimplifyPath.cpp
+++ LeetCode/c++/SimplifyPath.cpp
@@ -5,109 +5,164 @@
 
 using namespace std;
 
-string pathHelper(stack<string> st)
+//方法一：
+//string pathHelper(stack<string> st)
+//{
+//	string s="";
+//
+//	stack<string> newSt;
+//
+//	while (!st.empty())
+//	{
+//		string tmp = st.top();
+//		st.pop();
+//		newSt.push(tmp);
+//	}
+//
+//	while (!newSt.empty())
+//	{
+//		string tmp = newSt.top();
+//		newSt.pop();
+//		s += '/'+tmp;
+//	}
+//
+//	if (s.size() == 0)
+//		s = "/";
+//
+//	return s;
+//}
+//
+//string simplifyPath(string path) {
+//	if (path.size() == 0)
+//		return path;
+//	
+//	stack<string> st;
+//
+//	string dir = "";
+//	int dotCnt = 0; //一个点发现
+//	bool isSlashFound = false; //是否已经出现过slash
+//
+//	for (int i = 1; i < path.size(); i++)
+//	{
+//		if (path[i] == '/')
+//		{
+//			if (dotCnt == 2) //第二次点
+//			{
+//				if (!st.empty())
+//					st.pop();
+//			}
+//
+//			if (!isSlashFound)
+//			{
+//				isSlashFound = true;
+//
+//				if (dotCnt > 2)
+//				{
+//					for (int i = 0; i < dotCnt; i++)
+//						dir += '.';
+//				}
+//
+//				if(dir.size() != 0)
+//					st.push(dir);
+//			}
+//
+//			dir = "";
+//			dotCnt = 0;
+//		}
+//		else if (path[i] == '.')
+//		{
+//			if (dir.size() != 0)
+//				dir += '.';
+//			else
+//				dotCnt++;
+//		}
+//		else
+//		{
+//			for (int i = 0; i < dotCnt; i++)
+//				dir += '.';
+//
+//			dir += path[i];
+//			isSlashFound = false;
+//			dotCnt = 0;
+//		}
+//	}
+//
+//	if (dir.size() != 0) //处理/abc/与/abc这样的情况
+//		st.push(dir);
+//
+//	if (dotCnt > 2)
+//	{
+//		string dotStr = "";
+//		for (int i = 0; i < dotCnt; i++)
+//			dotStr += '.';
+//
+//		st.push(dotStr);
+//	}
+//	else if (dotCnt == 2)
+//	{
+//		if (!st.empty())
+//			st.pop();
+//	}
+//
+//
+//	//将栈中的元素转化成字符串
+//	string result = pathHelper(st);
+//
+//	return result;
+//}
+
+//方法2：
+vector<string> split(string path,char c)
 {
-	string s="";
+	vector<string> v;
+	if (path.size() == 0)
+		return v;
 
-	stack<string> newSt;
-
-	while (!st.empty())
+	int index = 0,nextIndex=0;
+	while (index < path.size())
 	{
-		string tmp = st.top();
-		st.pop();
-		newSt.push(tmp);
+		nextIndex = path.find(c,index+1);
+		string substr = path.substr(index+1,nextIndex-index-1);
+		v.push_back(substr);
+
+		index = nextIndex;
 	}
 
-	while (!newSt.empty())
-	{
-		string tmp = newSt.top();
-		newSt.pop();
-		s += '/'+tmp;
-	}
-
-	if (s.size() == 0)
-		s = "/";
-
-	return s;
+	return v;
 }
 
 string simplifyPath(string path) {
-	if (path.size() == 0)
-		return path;
-	
-	stack<string> st;
+	vector<string> splitV = split(path,'/');
+	vector<string> storedV;
 
-	string dir = "";
-	int dotCnt = 0; //一个点发现
-	bool isSlashFound = false; //是否已经出现过slash
-
-	for (int i = 1; i < path.size(); i++)
+	for (auto dir : splitV)
 	{
-		if (path[i] == '/')
+		if(dir.compare("") == 0 || dir.compare(".") == 0)//空和.就忽略
+		{}
+		else if (dir.compare("..") == 0)
 		{
-			if (dotCnt == 2) //第二次点
-			{
-				if (!st.empty())
-					st.pop();
-			}
-
-			if (!isSlashFound)
-			{
-				isSlashFound = true;
-
-				if (dotCnt > 2)
-				{
-					for (int i = 0; i < dotCnt; i++)
-						dir += '.';
-				}
-
-				if(dir.size() != 0)
-					st.push(dir);
-			}
-
-			dir = "";
-			dotCnt = 0;
-		}
-		else if (path[i] == '.')
-		{
-			if (dir.size() != 0)
-				dir += '.';
-			else
-				dotCnt++;
+			if (!storedV.empty())
+				storedV.pop_back();
 		}
 		else
-		{
-			for (int i = 0; i < dotCnt; i++)
-				dir += '.';
-
-			dir += path[i];
-			isSlashFound = false;
-			dotCnt = 0;
-		}
+			storedV.push_back(dir);
 	}
 
-	if (dir.size() != 0) //处理/abc/与/abc这样的情况
-		st.push(dir);
+	string returnStr = "";
+	for (auto str : storedV)
+		returnStr += "/" + str;
 
-	if (dotCnt > 2)
-	{
-		string dotStr = "";
-		for (int i = 0; i < dotCnt; i++)
-			dotStr += '.';
+	if (returnStr.size() == 0)
+		returnStr = "/";
 
-		st.push(dotStr);
-	}
-	else if (dotCnt == 2)
-	{
-		if (!st.empty())
-			st.pop();
-	}
+	return returnStr;
+}
 
-
-	//将栈中的元素转化成字符串
-	string result = pathHelper(st);
-
-	return result;
+void testPrint(string s)
+{
+	vector<string> v = split(s,'/');
+	for (auto word : v)
+		cout << word << endl;
 }
 
 int main()
@@ -145,6 +200,7 @@ int main()
 	cout << simplifyPath("///TJbrd/owxdG//") << endl;
 	cout << simplifyPath("///eHx/..") << endl;
 	cout << simplifyPath("///eHx/../") << endl;
+
 
 	return 0;
 }
