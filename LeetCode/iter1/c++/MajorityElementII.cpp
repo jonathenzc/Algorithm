671e94e4a129d16bf86a75979ea73dd424ad626b
Cheng,Zhang <459309661@163.com>
2016-07-31 19:29:16 +0800

    Add Majority Element II C++ version

---
+++ LeetCode/iter1/c++/MajorityElementII.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+	//数量大于n/3的只有可能是两个数
+	int candidate1 = -1, candidate2 = -1;
+	int cnt1 = 0,cnt2=0;
+	
+	//先找出两个候选数
+	for (int i = 0; i < nums.size();i++)
+	{
+		if (nums[i] == candidate1)
+			cnt1++;
+		else if (nums[i] == candidate2)
+			cnt2++;
+		else if (cnt1 == 0)
+		{
+			candidate1 = nums[i];
+			cnt1 = 1;
+		}
+		else if (cnt2 == 0)
+		{
+			candidate2 = nums[i];
+			cnt2 = 1;
+		}
+		else
+		{
+			cnt1--;
+			cnt2--;
+		}
+	}
+
+	//统计数量
+	cnt1 = 0, cnt2 = 0;
+	for (auto num : nums)
+	{
+		if (num == candidate1)
+			cnt1++;
+
+		if (num == candidate2)
+			cnt2++;
+	}
+
+	//判断数量是否超过nums的大小
+	vector<int> v;
+	if (cnt1 > nums.size() / 3)
+		v.push_back(candidate1);
+
+	if (candidate1 != candidate2 && cnt2 > nums.size() / 3)
+		v.push_back(candidate2);
+
+	return v;
+}
+
+int main()
+{
+	//vector<int> v;
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(2);
+	//v.push_back(1);
+	//v.push_back(1);
+	//v.push_back(1);
+	//v.push_back(3);
+	//v.push_back(4);
+
+	//[1,2,2,3,2,1,1,3]
+	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(2);
+	v.push_back(1);
+	v.push_back(1);
+	v.push_back(3);
+
+
+	vector<int> retV = majorityElement(v);
+	for (int i = 0; i < retV.size(); i++)
+		cout << retV[i] << " ";
+
+	return 0;
+}
