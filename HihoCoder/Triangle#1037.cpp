1fe9c4a7143786ef1a425ba124d014fbfdbaf2a3
Cheng,Zhang <459309661@163.com>
2016-08-23 16:34:34 +0800

    Add hiho coder 1037 C++ version

---
+++ HihoCoder/Triangle#1037.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int height;
+	cin >> height;
+
+	int *dp = new int[height]{0};
+	int *tmp = new int[height] {0};
+	int *coupon = new int[1];
+	cin >> coupon[0];
+
+	dp[0] = coupon[0];
+	tmp[0] = coupon[0];
+
+	int i = 2;
+	while (i <= height)
+	{
+		for (int j = 0; j < i; j++)
+			cin >> coupon[j];
+
+		for (int j = i-1; j>=0 ; j--)
+		{
+			if (j == 0)
+				dp[j] += coupon[j];
+			else
+				dp[j] = max(dp[j]+coupon[j],dp[j-1]+coupon[j]);
+		}
+
+		i++;
+	}
+
+
+	int maxAns = 0;
+
+	for (int i = 0; i < height; i++)
+		maxAns = max(maxAns,dp[i]);
+
+	cout << maxAns;
+
+
+	return 0;
+}
