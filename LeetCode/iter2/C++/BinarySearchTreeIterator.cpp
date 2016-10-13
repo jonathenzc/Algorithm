ed81b7113595a0b24d52cf1afdfd2cd37260c19d
Cheng,Zhang <459309661@163.com>
2016-10-13 12:08:58 +0800

    update binary search tree iterator C++ version

--- LeetCode/iter2/C++/BinarySearchTreeIterator.cpp
+++ LeetCode/iter2/C++/BinarySearchTreeIterator.cpp
@@ -1,42 +1,44 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <sstream>
-#include <unordered_map>
-#include <unordered_set>
-#include <utility>
-#include <queue>
-
-using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class BSTIterator {
 public:
 	BSTIterator(TreeNode *root) {
-		InOrderHelper(root);
+		//InOrderHelper(root);
+
+		pushLeftSubTree(root);
 	}
 
 	/** @return whether we have a next smallest number */
 	bool hasNext() {
-		return !q.empty();
+		return !st.empty();
 	}
 
 	/** @return the next smallest number */
 	int next() {
+		TreeNode *topNode = st.back();
+		st.pop_back();
+
+		if (topNode->right != NULL)
+			pushLeftSubTree(topNode->right);
+
+		return topNode->val;
+	}
+
+	//使用队列
+	/** @return whether we have a next smallest number */
+	bool hasNextQueue() {
+		return !q.empty();
+	}
+
+	/** @return the next smallest number */
+	int nextQueue() {
 		int val = q.front();
 		q.pop();
 
 		return val;
 	}
 private:
-	queue<int> q;
+	queue<int> q; //时间：O(1)，空间：O(n)
+
+	vector<TreeNode*> st; //时间：O(1),空间O(h)
 
 	void InOrderHelper(TreeNode *root)
 	{
@@ -47,30 +49,13 @@ private:
 			InOrderHelper(root->right);
 		}
 	}
+
+	void pushLeftSubTree(TreeNode *root)
+	{
+		while (root != NULL)
+		{
+			st.push_back(root);
+			root = root->left;
+		}
+	}
 };
-
-int main(void)
-{
-	TreeNode *root = new TreeNode(4);
-	TreeNode *node1 = new TreeNode(2);
-	TreeNode *node2 = new TreeNode(6);
-	TreeNode *node3 = new TreeNode(1);
-	TreeNode *node4 = new TreeNode(3);
-	TreeNode *node5 = new TreeNode(5);
-	TreeNode *node6 = new TreeNode(7);
-
-	root->left = node1;
-	root->right = node2;
-
-	node1->left = node3;
-	node1->right = node4;
-
-	node2->left = node5;
-	node2->right = node6;
-
-	BSTIterator i = BSTIterator(root);
-	while (i.hasNext()) 
-		cout << i.next()<<" ";
-
-	return 0;
-}
